8266bc6e43516a80c46f94828fea219e62e22f20
kawa <yuuya.apple@me.com>
2024-09-15 21:51:02 +0900

    railway

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,10 +18,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'railsapitester-production.up.railway.app'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
 end
